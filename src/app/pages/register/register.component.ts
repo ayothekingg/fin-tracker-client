b523f9be334c30810ef7a3bc8c6684fd9a8c5df9
ayothekingg <ayojames444@gmail.com>
2025-12-11 10:47:30 +0100

    (feat): register functionality implemented

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {
+  activeTab: 'login' | 'signup' = 'signup';
+
+  setActiveTab(tab: 'login' | 'signup') {
+    this.activeTab = tab;
+
+    if (tab === 'login') {
+      this.router.navigate(['/login']);
+    }
+
+    if (tab === 'signup') {
+      this.router.navigate(['/register']);
+    }
+  }
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private fBuilder: FormBuilder
+  ) {}
+
+  registerForm = this.fBuilder.group({
+    fullName: ['', [Validators.required, Validators.minLength(4)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  getErrorMessage(fieldName: string): string {
+    const control = this.registerForm.get(fieldName);
+    if (!control || !control.errors || !control.touched) return '';
+
+    if (control.errors['required']) return `${fieldName} is required`;
+    if (control.errors['minlength']) {
+      const minLength = control.errors['minlength'].requiredLength;
+      return `${fieldName} must be at least ${minLength} characters`;
+    }
+    if (control.errors['email']) return 'Please enter a valid email';
+    return '';
+  }
+
+  submit() {
+    const { fullName, email, password } = this.registerForm.value;
+    this.auth
+      .register({
+        fullName,
+        email,
+        password,
+      })
+      .subscribe({
+        next: () => this.router.navigate(['/login']),
+        error: (err) => console.log(err.error.message),
+      });
+  }
+}
