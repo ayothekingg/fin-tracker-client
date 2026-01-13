d6509bd384bfa93f284a97ec4789c198ede9bcd6
ayothekingg <ayojames444@gmail.com>
2026-01-13 09:48:45 +0100

    (feat): page routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AddExpenseComponent } from './pages/add-expense/add-expense.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: '',
+    component: LandingComponent,
+  },
   { path: 'login', component: LoginComponent },
   {
     path: 'register',
@@ -13,5 +18,9 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+  },
+  {
+    path: 'add-expense',
+    component: AddExpenseComponent,
   },
 ];
